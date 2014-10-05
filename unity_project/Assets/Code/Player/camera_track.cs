6bf907b02e3be4d10ad1972d557de97842359975
samsuite <suitester@gmail.com>
2014-10-04 21:50:45 -0400

    Added basic driving physics

---
+++ unity_project/Assets/Code/Player/camera_track.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_track : MonoBehaviour {
+
+	public GameObject targ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (Mathf.Lerp (transform.position.x, targ.transform.position.x, 100f*Time.deltaTime), Mathf.Lerp (transform.position.y, targ.transform.position.y, 100f*Time.deltaTime), -10f);
+		//transform.rotation = targ.transform.rotation;//= Quaternion.Lerp(transform.rotation, targ.transform.rotation, 0.1f);
+	}
+}
--- unity_project/Assets/Code/Player/player_movement.cs
+++ unity_project/Assets/Code/Player/player_movement.cs
@@ -3,22 +3,30 @@ using System.Collections;
 
 public class player_movement : MonoBehaviour {
 
+	private Rigidbody2D rb;
+
 	private Vector3 vel_vec = new Vector3(0,0,0);
 	private float accel = 0f;
 	private float brake = 0f;
 	private float h_axis = 0f;
 	private float v_axis = 0f;
-	public float p_vel = 0f;
+	public float speed = 0f;
+	private float disp = 0f;
 
-	private const float max_vel = 1f;
+	private const float max_vel = 20f;
+	private const float friction = 0.9f;
+	private const float rot_friction = 0.9f;
+
 	private float framespeed = 0f;
+	private float rot_vel = 0f;
 
-	// Use this for initialization
-	void Start () {
+	public GameObject cam;
+	Vector3 cam_offset = new Vector3(0,0,-10f);
 
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		accel = Input.GetAxis("right_trigger");
 		brake = Input.GetAxis ("left_trigger");
@@ -26,17 +34,29 @@ public class player_movement : MonoBehaviour {
 		v_axis = Input.GetAxis ("vertical_joystick_1");
 		framespeed = Time.deltaTime;
 
-		p_vel += accel/4f;
-		p_vel -= brake/2f;
-		if (p_vel > max_vel) {
-			p_vel = max_vel;
+		speed += (accel - brake)/2f;
+		if (accel < 0.5){
+			speed *= friction;
 		}
-		if (p_vel > 0) {
-			p_vel = 0;
+
+		if (speed > 20) {
+			speed = 20;
+		}
+		if (speed < -5) {
+			speed = -5;
 		}
 		
-		vel_vec.Set(0,0,-p_vel);
-		transform.Translate(vel_vec*framespeed);
-		//transform.Rotate();
+		vel_vec.Set(0,speed,0);
+		vel_vec = transform.rotation * vel_vec;
+		//transform.Translate(vel_vec*framespeed);
+		disp = Mathf.Sqrt(rb.velocity.magnitude + speed);
+
+		rb.AddTorque(-disp*h_axis*15f);
+		rb.AddForce(vel_vec*20f);
+
+		//cam.transform.position = transform.position;//Vector3.Lerp(cam.transform.position, transform.position, 20f*framespeed);
+		//cam.transform.position += cam_offset;
+		//cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, transform.rotation, 0.5f);
+
 	}
 }
