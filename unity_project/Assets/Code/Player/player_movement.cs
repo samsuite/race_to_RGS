9581fef1cc44fb3a8fe28f5dbf5876011a31baa4
samsuite <suitester@gmail.com>
2014-10-05 17:03:54 -0400

    Added gear shifting

--- unity_project/Assets/Code/Player/player_movement.cs
+++ unity_project/Assets/Code/Player/player_movement.cs
@@ -5,6 +5,9 @@ public class player_movement : MonoBehaviour {
 
 	private Rigidbody2D rb;
 
+	public int gear = 1;
+	public bool reverse = false;
+
 	private Vector3 vel_vec = new Vector3(0,0,0);
 	private float accel = 0f;
 	private float brake = 0f;
@@ -13,16 +16,19 @@ public class player_movement : MonoBehaviour {
 	public float speed = 0f;
 	private float disp = 0f;
 
-	private const float max_vel = 20f;
-	private const float friction = 0.9f;
+	private float max_vel = 20f;
+	private float min_vel = 20f;
+	private float rot_vel = 0f;
+	private float accel_rate = 5f;
+	private const float friction = 0.99f;
 	private const float rot_friction = 0.9f;
 
 	private float framespeed = 0f;
-	private float rot_vel = 0f;
 
 	public GameObject cam;
 	public GameObject lWheel;
 	public GameObject rWheel;
+
 
 	Vector3 cam_offset = new Vector3(0,0,-10f);
 
@@ -35,25 +41,57 @@ public class player_movement : MonoBehaviour {
 		brake = Input.GetAxis ("left_trigger");
 		h_axis = Input.GetAxis ("horizontal_joystick_1");
 		v_axis = Input.GetAxis ("vertical_joystick_1");
+
 		framespeed = Time.deltaTime;
 
 
-		if (accel > 0){
-			speed += (accel * (1f-brake))/2f;
+		if (gear == 1) {
+			max_vel = 5f;
+			min_vel = 0f;
+			accel_rate = 10f;
+		}
+		else if (gear == 2) {
+			max_vel = 15f;
+			min_vel = 5f;
+			accel_rate = 20f;
+		}
+		else if (gear == 3) {
+			max_vel = 30f;
+			min_vel = 15f;
+			accel_rate = 50f;
+		}
+
+		if (accel == 0) {
+			reverse = true;
+			speed -= brake/10f;
 		}
 		else{
-			speed -= brake;
+			reverse = false;
 		}
+
+		speed += (accel * (1f-brake))/accel_rate;
 
 		if (((accel * (1f-brake))/2f) < 0.5){
 			speed *= friction;
 		}
 
-		if (speed > 20) {
-			speed = 20;
+
+		if (speed > max_vel) {
+			if (gear < 3){
+				gear += 1;
+			}
+			else{
+				speed = max_vel;
+			}
 		}
-		if (speed < -5) {
-			speed = -5;
+		if (speed < min_vel) {
+			if (gear > 1){
+				gear -= 1;
+			}
+		}
+
+		if (speed < -3f){
+			speed = -3f;
 		}
 		
 		vel_vec.Set(0,speed,0);
