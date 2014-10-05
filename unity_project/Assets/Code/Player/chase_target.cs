4ee593ae49e7503473fa515a8fce9aebe77d94df
samsuite <suitester@gmail.com>
2014-10-05 02:44:58 -0400

    Better camera, turning wheels, bugfixes & tweaks

---
+++ unity_project/Assets/Code/Player/chase_target.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chase_target : MonoBehaviour {
+
+	public GameObject targ;
+	private NavMeshAgent navigator;
+
+	void Awake() {
+		navigator = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () {
+		navigator.SetDestination(targ.transform.position);
+	}
+}
--- unity_project/Assets/Code/Player/player_movement.cs
+++ unity_project/Assets/Code/Player/player_movement.cs
@@ -21,12 +21,15 @@ public class player_movement : MonoBehaviour {
 	private float rot_vel = 0f;
 
 	public GameObject cam;
+	public GameObject lWheel;
+	public GameObject rWheel;
+
 	Vector3 cam_offset = new Vector3(0,0,-10f);
 
 	void Awake() {
 		rb = GetComponent<Rigidbody2D>();
 	}
-	
+
 	void Update () {
 		accel = Input.GetAxis("right_trigger");
 		brake = Input.GetAxis ("left_trigger");
@@ -34,8 +37,15 @@ public class player_movement : MonoBehaviour {
 		v_axis = Input.GetAxis ("vertical_joystick_1");
 		framespeed = Time.deltaTime;
 
-		speed += (accel - brake)/2f;
-		if (accel < 0.5){
+
+		if (accel > 0){
+			speed += (accel * (1f-brake))/2f;
+		}
+		else{
+			speed -= brake;
+		}
+
+		if (((accel * (1f-brake))/2f) < 0.5){
 			speed *= friction;
 		}
 
@@ -49,14 +59,26 @@ public class player_movement : MonoBehaviour {
 		vel_vec.Set(0,speed,0);
 		vel_vec = transform.rotation * vel_vec;
 		//transform.Translate(vel_vec*framespeed);
-		disp = Mathf.Sqrt(rb.velocity.magnitude + speed);
+		disp = Mathf.Sqrt(Mathf.Abs(rb.velocity.magnitude + speed));
 
-		rb.AddTorque(-disp*h_axis*15f);
+		if (disp * h_axis != 0) {
+			if (speed > 0) {
+				rb.AddTorque (-disp * h_axis * 15f);
+			} else {
+				rb.AddTorque (disp * h_axis * 15f);
+			}
+		}
 		rb.AddForce(vel_vec*20f);
 
-		//cam.transform.position = transform.position;//Vector3.Lerp(cam.transform.position, transform.position, 20f*framespeed);
-		//cam.transform.position += cam_offset;
-		//cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, transform.rotation, 0.5f);
+		rWheel.transform.localEulerAngles = new Vector3(0f,0f,h_axis * -30);
+		lWheel.transform.localEulerAngles = new Vector3(0f,0f,h_axis * -30);
 
+		cam.transform.position = transform.position;
+		cam.transform.position += cam_offset;
+
+	}
+
+	void FixedUpdate () {
+		cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, transform.rotation, 0.25f);
 	}
 }
