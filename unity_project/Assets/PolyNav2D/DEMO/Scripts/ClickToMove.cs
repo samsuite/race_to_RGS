25e8c43701ade5271cebbb0adad4dffebc08cd39
samsuite <suitester@gmail.com>
2014-10-18 20:16:41 -0400

    Installed 2D Pathfinding Package

---
+++ unity_project/Assets/PolyNav2D/DEMO/Scripts/ClickToMove.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+//example
+[RequireComponent(typeof(PolyNavAgent))]
+public class ClickToMove : MonoBehaviour{
+	
+	private PolyNavAgent _agent;
+	public PolyNavAgent agent{
+		get
+		{
+			if (!_agent)
+				_agent = GetComponent<PolyNavAgent>();
+			return _agent;			
+		}
+	}
+
+	void Update() {
+
+		if (Input.GetMouseButtonDown(0))
+			agent.SetDestination(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+	}
+
+	//Message from Agent
+	void OnDestinationReached(){
+
+		//do something here...
+	}
+
+	//Message from Agent
+	void OnDestinationInvalid(){
+
+		//do something here...
+	}
+}
